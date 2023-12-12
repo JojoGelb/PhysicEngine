64aca1345d3b75e246d1a1013ec8c1772313593a
Jordy <jordygelb@gmail.com>
2023-12-12 11:04:00 -0500

    finished collision velocity modification

--- MathPhysicEngine/RigidbodyTest/RigidBodyContact.cpp
+++ MathPhysicEngine/RigidbodyTest/RigidBodyContact.cpp
@@ -10,21 +10,42 @@ void RigidBodyContact::Resolve(float duration)
 float RigidBodyContact::CalculateSeparatingVelocity()
 {
 	Vector3D relativeVelocity = rigidbody[0]->velocity;
-	if (rigidbody[1]) {
+	if (rigidbody[1])
+	{
 		relativeVelocity -= rigidbody[1]->velocity;
 	}
 
 	return relativeVelocity.DotProduct(contactNormal);
 }
 
-
-
-//Give proper impulse to solve contact
+// Give proper impulse to solve contact
 void RigidBodyContact::ResolveVelocity(float duration)
 {
 
+	Vector3D u1 = GetSpeedAtContactPoint(rigidbody[0]);
+	Vector3D u2 = GetSpeedAtContactPoint(rigidbody[1]);
 
-	
+	// Calculate vector between center of mass and point of contact
+	Vector3D r1 = contactPoint - rigidbody[0]->transform->position;
+	Vector3D r2 = contactPoint - rigidbody[1]->transform->position;
+
+	Vector3D urel = u1 - u2;
+
+	// Calculate impulse
+	Vector3D k = ((restitution + 1) * urel.DotProduct(contactNormal)) /
+				 ((rigidbody[0]->GetInversedMass() + rigidbody[1]->GetInversedMass()) * contactNormal +
+				  (rigidbody[0]->GetInverseInertiaTensorWorld() * r1.CrossProduct(contactNormal)).CrossProduct(r1) +
+				  ((rigidbody[1]->GetInverseInertiaTensorWorld() * r2.CrossProduct(contactNormal)).CrossProduct(r2)))
+					 .DotProduct(contactNormal);
+
+	// Calculate new velocity
+	rigidbody[0]->velocity = rigidbody[0]->velocity - k.DotProduct(contactNormal) / rigidbody[0]->GetMass();
+	rigidbody[1]->velocity = rigidbody[1]->velocity + k.DotProduct(contactNormal) / rigidbody[1]->GetMass();
+
+	// Calculate new angularVelocity
+	rigidbody[0]->angularVelocity = rigidbody[0]->angularVelocity - (rigidbody[0]->GetInverseInertiaTensorWorld() * (r1.CrossProduct(k.CrossProduct(contactNormal))));
+	rigidbody[1]->angularVelocity = rigidbody[1]->angularVelocity + (rigidbody[1]->GetInverseInertiaTensorWorld() * (r2.CrossProduct(k.CrossProduct(contactNormal))));
+
 	/*
 	//Velocity in direction of contact
 	float seperatingVelocity = CalculateSeparatingVelocity();
@@ -79,29 +100,35 @@ void RigidBodyContact::ResolveVelocity(float duration)
 void RigidBodyContact::ResolveInterpenetration()
 {
 
-	if (penetration <= 0) return;
+	if (penetration <= 0)
+		return;
 
 	float totalInversMass = rigidbody[0]->GetInversedMass();
-	if (rigidbody[1]) {
+	if (rigidbody[1])
+	{
 		totalInversMass += rigidbody[1]->GetInversedMass();
 	}
 
-	if (totalInversMass <= 0) return;
+	if (totalInversMass <= 0)
+		return;
 
 	Vector3D movePerIMass = contactNormal * (penetration / totalInversMass);
 
 	Vector3D particleAMovement = movePerIMass * rigidbody[0]->GetInversedMass();
 	Vector3D particleBMovement;
-	if (rigidbody[1]) {
+	if (rigidbody[1])
+	{
 		particleBMovement = movePerIMass * -rigidbody[1]->GetInversedMass();
 	}
-	else {
+	else
+	{
 		particleBMovement = Vector3D();
 	}
 
 	rigidbody[0]->transform->position += particleAMovement;
 
-	if (rigidbody[1]) {
+	if (rigidbody[1])
+	{
 		rigidbody[1]->transform->position += particleBMovement;
 	}
 
@@ -113,7 +140,6 @@ void RigidBodyContact::ResolveInterpenetration()
 		rigidbody[1]->position += movePerIMass * rigidbody[1]->GetInversedMass();
 	}*/
 
-	//PATCH: this line prevent the ResolveInterpenetration to be called several times during a frame
+	// PATCH: this line prevent the ResolveInterpenetration to be called several times during a frame
 	penetration = 0;
-
 }
--- MathPhysicEngine/RigidbodyTest/RigidBodyContact.h
+++ MathPhysicEngine/RigidbodyTest/RigidBodyContact.h
@@ -4,68 +4,46 @@
 
 class RigidBodyContact
 {
-	public:
-		// 1 could be nullptr
-		RigidBody* rigidbody[2];
+public:
+	// 1 could be nullptr
+	RigidBody *rigidbody[2];
 
-		//value between 0 - 1
-		//define the elasticness of the collision
-		// 1 = bouncy
-		// 0 = full stop on collision
-		float restitution;
+	// value between 0 - 1
+	// define the elasticness of the collision
+	//  1 = bouncy
+	//  0 = full stop on collision
+	float restitution;
 
-		// distance of penetration of the contact
-		float penetration;
+	// distance of penetration of the contact
+	float penetration;
 
-		Vector3D contactNormal;
+	Vector3D contactNormal;
 
-		Vector3D contactPoint;
+	Vector3D contactPoint;
 
-		//Solve Velocity and interpenetration
-		void Resolve(float duration);
+	// Solve Velocity and interpenetration
+	void Resolve(float duration);
 
-		float CalculateSeparatingVelocity();
+	float CalculateSeparatingVelocity();
 
-	private:
-		// Handle impulse for collision
-		void ResolveVelocity(float duration);
+private:
+	// Handle impulse for collision
+	void ResolveVelocity(float duration);
 
-		//Handle interpenetration for collision
-		void ResolveInterpenetration();
-
+	// Handle interpenetration for collision
+	void ResolveInterpenetration();
 
 	// * being cross product
-	//u = r + w * r : v linear velocity, w angular velocity, r vector from center of mass to point of contact
-	Vector3D VitessePointContact(RigidBody* rb) {
-		//maybe cross product
-		Vector3D result = rb->velocity + rb->angularVelocity.CrossProduct((contactPoint - rb->transform->position));
+	// u = r + w * r : v linear velocity, w angular velocity, r vector from center of mass to point of contact
+	Vector3D GetSpeedAtContactPoint(RigidBody *rb)
+	{
+		// maybe cross product
+		return rb->velocity + rb->angularVelocity.CrossProduct((contactPoint - rb->transform->position));
 	}
 
-	//Calculate k 
-	Vector3D GetImpulse() {
-
-		Vector3D u1 = VitessePointContact(rigidbody[0]);
-		Vector3D u2 = VitessePointContact(rigidbody[1]);
-
-		Vector3D r1 = contactPoint - rigidbody[0]->transform->position;
-		Vector3D r2 = contactPoint - rigidbody[1]->transform->position;
-
-		Vector3D urel = u1 - u2;
-
-		Vector3D result = ((restitution+1)* urel.DotProduct(contactNormal)) / 
-			(
-				(rigidbody[0]->GetInversedMass() + rigidbody[1]->GetInversedMass())*contactNormal + 
-				(rigidbody[0]->GetInverseInertiaTensorWorld() * r1.CrossProduct(contactNormal)).CrossProduct(r1) +
-				((rigidbody[1]->GetInverseInertiaTensorWorld() * r2.CrossProduct(contactNormal)).CrossProduct(r2))
-			).DotProduct(contactNormal);
+public:
+	RigidBodyContact(RigidBody *rigidBodyA = nullptr, RigidBody *rigidBodyB = nullptr, float restitution = 0, float penetration = 0, Vector3D contactNormal = Vector3D())
+		: rigidbody{rigidBodyA, rigidBodyB}, restitution(restitution), penetration(penetration), contactNormal(contactNormal)
+	{
 	}
-
-
-	public:
-
-		RigidBodyContact(RigidBody* rigidBodyA = nullptr, RigidBody* rigidBodyB = nullptr, float restitution = 0, float penetration = 0, Vector3D contactNormal = Vector3D())
-			: rigidbody{ rigidBodyA, rigidBodyB }, restitution(restitution), penetration(penetration), contactNormal(contactNormal)
-		{
-		}
 };
-
